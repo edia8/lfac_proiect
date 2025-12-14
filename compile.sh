d9561eb2f5245115337797049180b1811cd94d64
edia8 <eaioanei14@gmail.com>
2025-12-15 01:39:54 +0200

    script pt rulare

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    FILE="limbaj"
+else
+    FILE="$1"
+fi
+rm -f lex.yy.c parser.tab.c parser.tab.h
+bison -d parser.y
+flex scanner.l
+g++ lex.yy.c parser.tab.c -o "$FILE"
+./"$FILE" < input.txt
